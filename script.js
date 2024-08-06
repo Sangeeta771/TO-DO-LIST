3d225cf775546e525edd396095ea0860a7e3e1cd
Sangeeta Jena <162555238+Sangeeta771@users.noreply.github.com>
2024-08-06 15:15:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,66 @@
-let addToDoButton = document.getElementById('addToDo');
-let toDoContainer = document.getElementById('toDoContainer');
-let inputField = document.getElementById('inputField');
+ const inputBox=document.getElementById('inputBox');
+ const listContainer=document.getElementById("listContainer");
 
-addToDoButton.addEventListener('click', function () {
-    var paragraph = document.createElement('p');
-    paragraph.classList.add('paragraph-styling');
-    paragraph.innerText = inputField.value;
-    toDoContainer.appendChild(paragraph);
-    inputField.value = " ";
-    paragraph.addEventListener('click', function () {
-        paragraph.style.textDecoration = "line-through";
-    })
-    paragraph.addEventListener('dblclick', function () {
-        toDoContainer.removeChild(paragraph);
-    })
-})
+ let addTask=()=>{
+    if(inputBox.value===''){
+        alert('you must write something to add..')
+    }
+    else{
+        let li=document.createElement('li');
+        li.innerHTML=`<p>${inputBox.value}</p> <i id="cross" class="fa-solid fa-xmark"></i>`;
+        listContainer.appendChild(li);   
+    }
+    inputBox.value='';
+    saveData()
+ }
+
+listContainer.addEventListener('click',function (e) {
+    if(e.target.tagName==='LI'){
+        e.target.classList.toggle('checked')
+        saveData()
+    }
+    if(e.target.id==='cross'){
+        e.target.parentElement.remove()
+        saveData();
+    }
+    
+},false)
+
+
+function saveData() {
+    localStorage.setItem("data", listContainer.innerHTML)
+}
+
+
+// This is for Keyboard event ....
+
+        inputBox.addEventListener("keydown", function(e) {
+            if (e.key=== "Enter") {
+                addKeyTask();
+            }
+        });
+
+        function addKeyTask() {
+            if(inputBox.value===''){
+                alert('you must write something to add..')
+            }
+            else{
+                let li=document.createElement('li');
+                li.innerHTML=`<p>${inputBox.value}</p> <i id="cross" class="fa-solid fa-xmark"></i>`;
+                listContainer.appendChild(li);   
+            }
+            inputBox.value='';
+            saveData()
+            
+        }
+
+
+        function saveData() {
+            localStorage.setItem("data", listContainer.innerHTML);
+        }
+        
+        function showTask(){
+            listContainer.innerHTML=localStorage.getItem("data");
+        }
+
+        showTask();
